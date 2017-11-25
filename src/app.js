9b51ab30891ffe8614d8192811e36585b80800df
Dan Roberts <dan@adadevelopersacademy.org>
2017-11-25 00:26:43 -0800

    All collection management (sort, filter, select one) works
    This also has a starting crack at building a generic managed form
    object, but it definitely doesn't work yet. I'm too sleepy to make good
    progress here, so signing off. Probably would be finished already if I
    had just special-cased each of them.

--- src/app.js
+++ src/app.js
@@ -14,7 +14,30 @@ import TripCollection from 'collections/trip_collection';
 // ======================
 const TRIP_FIELDS = ['name', 'category', 'continent', 'weeks', 'cost'];
 const tripList = new TripCollection();
+let selectedTrip;
 
+const selectTrip = function(trip) {
+  if (selectedTrip) {
+    selectedTrip.set('selected', false);
+  }
+  trip.set('selected', true);
+  selectedTrip = trip;
+};
+
+const tripSelected = function(trip) {
+  return selectedTrip == trip;
+};
+
+const genericBackboneErrorHandler = function(target, response) {
+  if (response.readyState == 4) {
+    console.log('Got regular type error');
+    console.log(response);
+    reportStatus('error', response.responseText);
+  } else {
+    console.log('Request did not complete round-trip! Networking error?');
+    reportStatus('error', 'Could not connect to server');
+  }
+}
 
 // ======================
 // Underscore template management
@@ -62,7 +85,13 @@ const buildTripTable = function() {
   const headerRow = $('#trip-table thead tr');
   const filterDropDown = $('#trip-table .filter select');
   TRIP_FIELDS.forEach((field) => {
-    headerRow.append(TEMPLATES.tripTableHeader({ field: field }));
+    const header = $(TEMPLATES.tripTableHeader({ field: field }));
+    header.on('click', function(event) {
+      console.log(`Sorting by ${ field }`);
+      tripList.comparator = field;
+      tripList.sort();
+    });
+    headerRow.append(header);
     filterDropDown.append(`<option value="${ field }">${ field }</option>`);
   });
 };
@@ -71,13 +100,55 @@ const renderTripTable = function(tripList) {
   const tripTable = $('#trip-table tbody');
   tripTable.html('');
 
-  tripList.forEach((trip) => {
-    const generatedHTML = TEMPLATES.tripSummary({
+  tripList.getFiltered().forEach((trip) => {
+    const generatedHTML = $(TEMPLATES.tripSummary({
       fields: TRIP_FIELDS,
       trip: trip
+    }));
+    // dat closure doe
+    generatedHTML.on('click', function(event) {
+      selectTrip(trip);
+      trip.fetch({
+        success: renderTripDetails,
+        error: genericBackboneErrorHandler
+      });
+      $('#trip-table tbody tr').removeClass('selected');
+      $(this).addClass('selected');
     });
     tripTable.append(generatedHTML);
   });
+
+  // Apply styling to the current sort field
+  $('#trip-table th.sort').removeClass('selected');
+  $(`#trip-table th.sort.${ tripList.comparator }`).addClass('selected');
+};
+
+const filterTrips = function(event) {
+  console.log(`Filtering trips. Keycode is ${ event.keyCode }`);
+  if (event.keyCode == 27) {
+    // ESC key. Reset the filter.
+    $('#trip-table .filter input').val('');
+  }
+
+  const field = $('#trip-table .filter-field').val();
+  const value = $('#trip-table .filter input[name="filter-value"]').val();
+  tripList.filter(field, value);
+};
+
+// ======================
+// Trip details management
+// ======================
+const renderTripDetails = function(trip, element) {
+  // There's a potential race condition here where two selections
+  // come in quick succession, and the second gets handled before the first.
+  if (!tripSelected(trip)) {
+    return;
+  }
+
+  const generatedHTML = $(TEMPLATES.tripDetails({trip: trip}));
+  generatedHTML.find('.register').append(TEMPLATES.managedForm({ fields: trip.fields, submitText: 'Reserve'}));
+  $('#trip-details').html(generatedHTML);
+  $('#trip-details').show();
 };
 
 // ======================
@@ -89,11 +160,15 @@ $(document).ready( () => {
 
   // Register random one-off DOM event handlers
   $('#status-messages button.clear').on('click', clearStatus);
+  $('#trip-table tbody tr').on('click', renderTripDetails);
+  $('#trip-table .filter input').on('keyup', filterTrips);
+  $('#trip-table .filter-field').on('change', filterTrips);
+  $('#trip-table .filter').on('submit', event => event.preventDefault());
 
   buildTripTable();
 
   // Register event handlers on our trip list
-  tripList.on('update', renderTripTable);
+  tripList.on('update sort filter', renderTripTable);
 
   // Start fetching trip data
   console.log('About to fetch data');
@@ -103,15 +178,6 @@ $(document).ready( () => {
     success: (collection, response) => {
       console.log(`Successfully fetched ${collection.length} trips`);
     },
-    error: (collection, response) => {
-      if (response.readyState == 4) {
-        console.log('Got regular type error');
-        console.log(response);
-        reportStatus('error', response.responseText);
-      } else {
-        console.log('Request did not complete round-trip! Networking error?');
-        reportStatus('error', 'Could not connect to server');
-      }
-    }
+    error: genericBackboneErrorHandler
   });
 });
--- src/collections/trip_collection.js
+++ src/collections/trip_collection.js
@@ -3,7 +3,49 @@ import Trip from 'models/trip';
 
 const TripCollection = Backbone.Collection.extend({
   model: Trip,
-  url: 'http://localhost:3000/trips/'
+  url: 'http://localhost:3000/trips/',
+  TEXT_FIELDS: ['name', 'category', 'continent'],
+  NUMERIC_FIELDS: ['weeks', 'cost'],
+
+  filter(field, value) {
+    this.filterField = field;
+    if (this.TEXT_FIELDS.includes(field)) {
+      this.filterValue = value.toLowerCase();
+    } else if (this.NUMERIC_FIELDS.includes(field)) {
+      this.filterValue = parseFloat(value);
+    } else {
+      // Shouldn't be possible with current UI design
+      throw `Invalid filter field ${ this.filterField }`;
+    }
+    this.trigger('filter', this);
+  },
+
+  getFiltered() {
+    // Perf is exceptionally important here, since this runs every keystroke
+    // It was pretty snappy on my machine with only 30 trips, but YMMV.
+    // Things to look into if perf is an issue:
+    //   - cache lowercase / integer versions of fields on models
+    //   - track state between filters - if they previously filtered for
+    //       'abc' and now for 'abcd', only look in the previous results
+    //   - track whether we've got a numeric or text filter
+    //       (avoid the strcmp of includes)
+    if (!this.filterField || !this.filterValue) {
+      return this;
+    }
+
+    if (this.TEXT_FIELDS.includes(this.filterField)) {
+      return this.select((trip) => {
+        return trip.get(this.filterField).toLowerCase().includes(this.filterValue);
+      });
+    } else if (this.NUMERIC_FIELDS.includes(this.filterField)) {
+      return this.select((trip) => {
+        // console.log(`${ trip.get(this.filterField) } ${ parseFloat(trip.get(this.))}`);
+        return parseFloat(trip.get(this.filterField)) <= this.filterValue;
+      });
+    }
+    // how did we get here?
+    throw `Invalid filter field ${ this.filterField }`;
+  }
 });
 
 export default TripCollection;
