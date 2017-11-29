6cab10e52fc23c5ba9f8ace36a9d5adec9567235
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-28 21:38:02 -0800

    can access new attributes for model after fetch for trip detauls

--- src/app.js
+++ src/app.js
@@ -31,6 +31,23 @@ const render = function render(tripList) {
     // append the tr with HTML for each trip to the tbody
     tripListElement.append($(tripHTML));
   }) // forEach
+
+  // click event to get the details for a trip
+  $('.trip').on('click', function(event) {
+    console.log('in the trip click');
+
+    // get the id of the trip you clicked on
+    let tripId = $(this).attr('data-id')
+
+    // fetch the trip details of the trip you clicked on from the api
+    // the new attributes fetch sets for the model need to be access in a callback function
+    let trip = tripList.get(tripId)
+    trip.fetch({
+      success: function(model) {
+        // console.log(`in the fetch and about is: ${model.get('about')}`);
+      } // function
+    }); // fetch
+  })
 } // render
 
 // function to show all the trips when the 'Explore our trips' button is clicked
@@ -42,6 +59,13 @@ const showAllTrips = function showAllTrips() {
   $(this).hide();
 }
 
+// function to show the trip details when a tr is clicked on
+const showTripDetails = function showTripDetails() {
+  console.log('got into the showTripDetails function!');
+
+  alert('in the showTripDetails function')
+} // showTripDetails
+
 $(document).ready(() => {
     // make the underscore function to list all the trips
     console.log('in .ready!');
@@ -50,7 +74,8 @@ $(document).ready(() => {
     // get the trips from the api when the user clicks the 'Explore our trips!'.
     $('#get-trips').on('click', showAllTrips)
 
-
     // when we update tripList we will rerender the page
     tripList.on('update', render)
+
+
   }); // .ready
--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -4,6 +4,8 @@ import Trip from '../models/trip';
 const TripList = Backbone.Collection.extend({
   model: Trip,
   url: 'https://ada-backtrek-api.herokuapp.com/trips',
+  // default comparator so it sorts by name to start off 
+  comparator: name,
 });
 
 export default TripList;
