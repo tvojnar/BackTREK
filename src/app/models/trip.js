864bba9818114e87cb0cec33b72c9cc826d6876c
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-29 16:54:06 -0800

    added validations to the Trip model

--- src/app/models/trip.js
+++ src/app/models/trip.js
@@ -1,5 +1,33 @@
 import Backbone from 'backbone'
 
-const Trip = Backbone.Model.extend({});
+const Trip = Backbone.Model.extend({
+  validate(attributes) {
+    const errors = {};
 
-export default Trip; 
+    if (!attributes.name) {
+      errors.name = ['cannot be blank'];
+    }
+
+    if (!attributes.continent) {
+      errors.continent = ['cannot be blank'];
+    }
+
+    if (!attributes.category) {
+      errors.category = ['cannot be blank'];
+    }
+
+    if (!attributes.about) {
+      errors.about = ['cannot be blank'];
+    }
+
+    if (!attributes.weeks) {
+      errors.weeks = ['cannot be blank'];
+    }
+
+    if (!attributes.cost) {
+      errors.cost = ['cannot be blank'];
+    }
+  } // validate
+});
+
+export default Trip;
