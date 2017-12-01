cd74486af69096d2b6c53390849f6a1a17f0fc52
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-01 14:13:45 -0800

    added if to handle filtering for category, name, and continent

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -12,12 +12,12 @@ const TripList = Backbone.Collection.extend({
     // TODO: add if/else to do differnt filters depending on what the input and option properties are
     if (Object.keys(this.filterValues).length > 0 ) {
       for (var key in this.filterValues) {
-        // if (this.filterValues[key]) {
+        if (key === 'continent' || key === 'category'  || key === 'name') {
           filtered = filtered.filter((model) => {
             console.log(`in filter! model: ${model}`);
             return model.get(key).includes(this.filterValues[key]);
           }) // _filter
-        // } // if 'continent'
+        } // if 'continent', 'name', or 'category'
       } // for loop
     } // if filterValues is empty
     return filtered;
