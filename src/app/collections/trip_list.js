f7d1970f582b2b8a6b3e84476389f7fa4c9011f7
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-01 13:08:38 -0800

    filter loop works for filtering category

--- src/app/collections/trip_list.js
+++ src/app/collections/trip_list.js
@@ -8,16 +8,21 @@ const TripList = Backbone.Collection.extend({
   comparator: name,
   filterData() {
     console.log('in filterData');
+    let filtered = this;
     // TODO: add if/else to do differnt filters depending on what the input and option properties are
-    if (this.filterValues['continent']) {
-      let filtered = this.filter((model) => {
-        console.log(`in filter! model: ${model}`);
-        return model.get('continent').includes(this.filterValues['continent']);
-      }) // _filter
-      // return a new collection that contains all the trips that passed the filter
-      return new TripList(filtered);
-    } // if 'continent'
-    return this;
+    if (Object.keys(this.filterValues).length > 0 ) {
+      for (var key in this.filterValues) {
+        if (this.filterValues[key]) {
+          filtered = filtered.filter((model) => {
+            console.log(`in filter! model: ${model}`);
+            return model.get('continent').includes(this.filterValues[key]);
+          }) // _filter
+          // return a new collection that contains all the trips that passed the filter
+          // return new TripList(filtered);
+        } // if 'continent'
+      } // for loop
+    } // if filterValues is empty
+    return filtered;
   }, // filterData,
   filterValues: {}, // set filter option
 });
